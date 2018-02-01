015e52995b704c8cf7ba4996a94f7f528ebc6e65
tcy <thomas.chevily@canaltp.fr>
2018-02-01 16:08:41 +0100

    Adding customer delete event name in available event names

--- Event/SamCoreEvents.php
+++ Event/SamCoreEvents.php
@@ -5,4 +5,5 @@ namespace CanalTP\SamCoreBundle\Event;
 final class SamCoreEvents {
     const CREATE_CLIENT = 'sam_core.event.client.create';
     const EDIT_CLIENT   = 'sam_core.event.client.edit';
+    const DELETE_CUSTOMER = 'sam_core.event.customer.delete';
 }
