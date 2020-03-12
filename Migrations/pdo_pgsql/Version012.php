b88fc80e2004483a9b4c667a91448b1072945443
Thomas Chevily <thomas.chevily@canaltp.fr>
2020-03-12 11:55:07 +0100

    Add usr_delete_time in user table

---
+++ Migrations/pdo_pgsql/Version012.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace CanalTP\SamCoreBundle\Migrations\pdo_pgsql;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Migrations\AbstractMigration;
+
+/**
+ * Adding created_at column in public.t_user_usr table
+ */
+class Version012 extends AbstractMigration
+{
+    const VERSION = '0.12.0';
+
+    public function getName()
+    {
+        return self::VERSION;
+    }
+
+    /**
+     * Adding timezone column into user table
+     *
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $sql = 'ALTER TABLE public.t_user_usr ';
+        $sql.= 'ADD COLUMN usr_delete_time timestamp(0) without time zone DEFAULT NULL::timestamp without time zone';
+
+        $this->addSql($sql);
+    }
+
+    /**
+     * Drop timezone column from user table
+     *
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE public.t_user_usr DROP COLUMN usr_delete_time');
+    }
+}
