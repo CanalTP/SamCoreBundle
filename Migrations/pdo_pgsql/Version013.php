67fe710d2d41130722714e94929b905a910d7f63
pthegner <pierre.thegner@canaltp.fr>
2020-09-02 14:23:13 +0200

    Apply suggestions from code review
    Co-authored-by: LAAJIMI Fahmi <fahmi.laajimi@kisio.com>

--- Migrations/pdo_pgsql/Version013.php
+++ Migrations/pdo_pgsql/Version013.php
@@ -25,7 +25,7 @@ class Version013 extends AbstractMigration
     public function up(Schema $schema)
     {
         $sql = 'UPDATE public.tr_application_app SET app_default_route=app_default_route || \'/\' ';
-        $sql .= 'WHERE length(app_default_route) - length(replace(app_default_route, '/', '')) = 1 ';
+        $sql .= 'WHERE length(app_default_route) - length(replace(app_default_route, \'/\', \'\')) = 1 ';
         $sql .= 'AND right(app_default_route, 1) <> \'/\'';
         $this->addSql($sql);
     }
@@ -38,7 +38,7 @@ class Version013 extends AbstractMigration
     public function down(Schema $schema)
     {
         $sql = 'UPDATE public.tr_application_app SET app_default_route=left(app_default_route, -1) ';
-        $sql .= 'WHERE length(app_default_route) - length(replace(app_default_route, '/', '')) = 2 ';
+        $sql .= 'WHERE length(app_default_route) - length(replace(app_default_route, \'/\', \'\')) = 2 ';
         $sql .= 'AND right(app_default_route, 1) = \'/\'';
         $this->addSql($sql);
     }
