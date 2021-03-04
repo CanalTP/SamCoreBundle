7c26ca6b394c832119c9307a743b0d4953a25a27
dvdn <david.negrello@canaltp.fr>
2021-03-04 16:49:35 +0100

    GDPR fix weird user case if no creationDate

--- Tests/Unit/Services/GDPR/FactoryTest.php
+++ Tests/Unit/Services/GDPR/FactoryTest.php
@@ -66,7 +66,8 @@ class FactoryTest extends GdprTestCase
             [$this->mockUser(14, $oneMonthLater, $sevenMonthAgo, $tenMonthAgo), Nothing::class],
             [$this->mockUser(15, $oneMonthAgo, $sevenMonthAgo, $fiveMonthAgo), Reset::class],
             [$this->mockUser(16, $oneMonthLater, $sevenMonthAgo, $oneMonthAgo), Reset::class],
-            [$this->mockUser(17, null, $oneMonthAgo, null), Nothing::class]
+            [$this->mockUser(17, null, null, null), Warning::class],
+            [$this->mockUser(18, null, $oneMonthAgo, null), Nothing::class]
         ];
     }
 
